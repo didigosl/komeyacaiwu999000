37903420d2c8f09bbb92764dcac492cdbabd6c49
Johnn <you@example.com>
2026-02-28 13:35:23 +0100

    Use DB env vars for server connection

--- server/src/db.js
+++ server/src/db.js
@@ -2,10 +2,19 @@ import pg from 'pg';
 
 const { Pool } = pg;
 
-const DATABASE_URL = process.env.DATABASE_URL || 'postgres://postgres@localhost:5432/komeya';
+const {
+  DATABASE_URL,
+  DB_HOST = 'localhost',
+  DB_PORT = '5432',
+  DB_USER = 'postgres',
+  DB_PASSWORD = '',
+  DB_NAME = 'komeya'
+} = process.env;
+
+const connectionString = DATABASE_URL || `postgres://${encodeURIComponent(DB_USER)}${DB_PASSWORD ? ':'+encodeURIComponent(DB_PASSWORD) : ''}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
 
 export const pool = new Pool({
-  connectionString: DATABASE_URL,
+  connectionString,
   max: 10
 });
 
