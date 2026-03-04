917aafb995fdf61949d55cfd89e516789abf7838
Johnn <you@example.com>
2026-03-04 23:13:28 +0100

    fix: disable api caching

--- server/src/app.js
+++ server/src/app.js
@@ -11,6 +11,7 @@ app.use(cors());
 app.use(express.json({ limit: '20mb' }));
 app.use(morgan('dev'));
 app.use((req, res, next) => {
+  res.setHeader('Cache-Control', 'no-store');
   if (!req.path.startsWith('/api/')) req.url = '/api' + req.url;
   next();
 });
